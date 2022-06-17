ca67f8c6fbaf786bfb7c68b8c7828eb690aca9df
kate <christiannparanas@gmail.com>
2022-06-18 01:20:57 +0800

    landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { FeedbacksComponent } from './pages/feedbacks/feedbacks.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LandingComponent } from './pages/landing/landing.component';
+
+
+const routes: Routes = [
+  { path: '', component: LandingComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'faqs', component: FaqsComponent },
+  { path: 'feedbacks', component: FeedbacksComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FaqsComponent } from './pages/faqs/faqs.component';
+import { FeedbacksComponent } from './pages/feedbacks/feedbacks.component';
+import { AuthComponent } from './admin/auth/auth.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    NavigationComponent,
+    FaqsComponent,
+    FeedbacksComponent,
+    AuthComponent,
+    DashboardComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
